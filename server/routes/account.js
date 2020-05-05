30ddac847485c206e571edc3a592dfc8bc8624c0
tushermahmud <sazzadmahmud01795355849@gmail.com>
2020-05-05 20:38:03 +0600

    address api and posfile post api

--- server/routes/account.js
+++ server/routes/account.js
@@ -68,12 +68,57 @@ router.post('/login',(req,res,next)=>{
     });
 });
 router.get('/profile',checkJWT,(req,res,next)=>{
-    console.log("tusher");
     User.findOne({_id:req.decoded.user._id},(err,user)=>{
         res.json({
             success:true,
             user:user,
             message:'successful!'
+        });
+    });
+});
+router.post('/profile',checkJWT,(req,res,next)=>{
+    User.findOne({_id:req.decoded.user._id},(err,user)=>{
+        if(err) res.json({
+            error:err
+        });
+        next();
+        if(req.body.name) user.name=req.body.name;
+        if(req.body.email) user.email=req.body.email;
+        if(req.body.password) user.password=req.body.password;
+        user.isSeller=req.body.isSeller;
+        user.save();
+        res.json({
+            success:true,
+            message:"user profile has been updated!"
+        });
+    });
+});
+router.get('/address',checkJWT,(req,res,next)=>{
+    User.findOne({_id:req.decoded.user._id},(err,address)=>{
+        res.json({
+            success:true,
+            address:address.address,
+            message:'successful!'
+        });
+    });
+});
+router.post('/address',checkJWT,(req,res,next)=>{
+    User.findOne({_id:req.decoded.user._id},(err,address)=>{
+        if(err) res.json({
+            error:err
+        });
+        next();
+        if(req.body.address1) address.address.address1=req.body.address1;
+        if(req.body.address2) address.address.address2=req.body.address2;
+        if(req.body.city) address.address.city=req.body.city;
+        if(req.body.state) address.address.state=req.body.state;
+        if(req.body.country) address.address.country=req.body.country;
+        if(req.body.postCode) address.address.postCode=req.body.postcode;
+
+        address.save();
+        res.json({
+            success:true,
+            message:"User address has been updated!"
         });
     });
 });
